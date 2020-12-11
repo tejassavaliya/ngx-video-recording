f26484abb046203bf46640348dfb5dc1d9ae82e4
Tejas Savaliya <tejas14883@gmail.com>
2020-12-12 00:03:28 +0530

    refactor name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-app'`, () => {
+  it(`should have as title 'ngx-video-recording'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('ngx-video-recording');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-video-recording!');
   });
 });
